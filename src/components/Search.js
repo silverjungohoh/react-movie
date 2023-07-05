f094a27f3f770c18c86a1337d830508f46f4e279
grace <grace994084@gmail.com>
2023-07-05 10:22:06 +0900

    feat : useRef 통한 DOM 요소 접근

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,25 @@
+import { useEffect, useRef } from "react";
+
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
+      if (e.code === "Enter") {
+        console.log(inputEl.current);
+        inputEl.current.focus();
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return () => {
+      document.removeEventListener("keydown", callback);
+      console.log("closing");
+    };
+  }, []);
+
   return (
     <input
       type="text"
@@ -6,6 +27,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     ></input>
   );
 }
